f3c3166087c243be73520ada86ad2ad4da2517f3
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-08-29 12:44:17 -0400

    able to get friend request from db. filtering out requests that are accepted and declined.

--- src/container/PendingRequest.js
+++ src/container/PendingRequest.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react'
+import config from '../config'
+
+import axios from 'axios'
+
+import Context from '../context/ProfileContext'
 
 export class PendingRequest extends Component {
-    render() {
+
+    static contextType = Context
+
+    RenderRequests = (friendRequest) => {
+        if(friendRequest.message){
+            console.log(friendRequest.message)
+        } else {
+            console.log('requests',friendRequest)
+            console.log('filtered',friendRequest.filter(item => item.accepted === null && item.declined === null))
+        }
+    }
+
+    componentDidMount(){
+        axios.get(`${config.API_ENDPOINT}/get/friendrequests/${this.context.globalProfile.id}`)
+            .then(res => this.RenderRequests(res.data))
+    }
+
+//if user declines request patch that request from db
+//so user who initiated the request can make a request again
+
+    render(){
         return (
             <div>
                 Pending friend Requests
