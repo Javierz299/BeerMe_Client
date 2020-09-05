61fcbf6e068cd68def1ab802ae2626ce90b9d32f
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-09-05 11:10:00 -0400

    axios call to get user drinks for graph data. from home route.

--- src/container/PublicGraph.js
+++ src/container/PublicGraph.js
@@ -1,43 +1,10 @@
 import React, { Component } from 'react'
-import config from '../config'
-import axios from 'axios'
-import { connect } from 'react-redux'
-import * as ACTIONS from '../store/actions/actions'
+
 
 import Graph from '../container/ConfigureGraph'
 
 
 class PublicGraph extends Component {
-
-    componentDidMount(){
-        axios.get(`${config.API_ENDPOINT}/get/alluserdata`)
-            .then(res => {
-                console.log('graph data',res.data)
-                let beerTotal = 0
-                let seltzerTotal = 0
-                let craftTotal = 0
-                let wineTotal = 0
-                let shotsTotal = 0
-                let mixedTotal = 0
-
-                for(let user of res.data){
-                    beerTotal += user.beer
-                    seltzerTotal += user.seltzer
-                    craftTotal += user.craft_beer
-                    wineTotal += user.wineTotal
-                    shotsTotal += user.shots
-                    mixedTotal += user.cocktail
-                }
-                this.props.cumulativeData([
-                    beerTotal,seltzerTotal,
-                    craftTotal,wineTotal,
-                    shotsTotal,mixedTotal
-                ])
-            })
-        
-    }
-
-
     render() {
         return (
             <div>
@@ -46,11 +13,4 @@ class PublicGraph extends Component {
         )
     }
 }
-
-function mapDispatchToProps(dispatch){
-    return {
-        cumulativeData: (drinks) => dispatch(ACTIONS.cumulative_drinks(drinks)),
-    }
-}
-
-export default connect (null,mapDispatchToProps)(PublicGraph)
+export default PublicGraph
