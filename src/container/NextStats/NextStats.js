1534987b1b8af8a8697cabd2c82ae73b5046f5c7
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-10-21 12:28:48 -0400

    changed class to functional to use context api

--- src/container/NextStats/NextStats.js
+++ src/container/NextStats/NextStats.js
@@ -1,36 +1,29 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import Context from '../../context/ProfileContext'
 import Loading from '../../loading/loading'
 import * as ACTIONS from '../../store/actions/actions'
 
 import { connect } from 'react-redux'
 
-class NextStats extends Component{
-    static contextType = Context
+const NextStats = () => {
+    const context = useContext(Context)
 
-render(){
     return (
         <div>
-            {!this.context.globalStats ?
+            {!context.globalStats ?
                     <Loading /> :
                 <div id="profile-stats-container">
-                    <div><span>Beer: {this.context.globalStats.beer}</span></div>
-                    <div><span>Seltzer: {this.context.globalStats.seltzer}</span></div>
-                    <div><span>Craft: {this.context.globalStats.craft_beer}</span></div>
-                    <div><span>Wine: {this.context.globalStats.wine}</span></div>
-                    <div><span>Shots: {this.context.globalStats.shots}</span></div>
-                    <div><span>Mixed: {this.context.globalStats.cocktail}</span></div>
+                    <div><span>Beer: {context.globalStats.beer}</span></div>
+                    <div><span>Seltzer: {context.globalStats.seltzer}</span></div>
+                    <div><span>Craft: {context.globalStats.craft_beer}</span></div>
+                    <div><span>Wine: {context.globalStats.wine}</span></div>
+                    <div><span>Shots: {context.globalStats.shots}</span></div>
+                    <div><span>Mixed: {context.globalStats.cocktail}</span></div>
                 </div>
                 }
         </div>
     )
   }
-}
 
-function mapDispatchToProps(dispatch){
-    return {
-        set_profile_stats: (profile) => dispatch(ACTIONS.set_profile_stats(profile)),
-    }
-}
 
-export default connect(null,mapDispatchToProps)(NextStats)
+export default NextStats
