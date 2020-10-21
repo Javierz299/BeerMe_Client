a50f090c5c4ebee742285aeb1a3e9dd529198bfc
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-10-21 13:09:21 -0400

    got rid of context in extra stats.

--- src/container/ExtraStats/ExtraStats.js
+++ src/container/ExtraStats/ExtraStats.js
@@ -1,10 +1,8 @@
-import React, { useContext } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import Context from '../../context/ProfileContext'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import Loading from '../../loading/loading'
 
 const ExtraStats= () => {
-    const context = useContext(Context)
     const state = useSelector(state => state.drinktwo_reducer)
     console.log("state",state)
     return (
