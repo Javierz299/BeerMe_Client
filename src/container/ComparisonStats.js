21e102f14acdfd59e2f81139bee216865f0cfef6
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-10-21 18:05:49 -0400

    addes pics and stats to friends details

--- src/container/ComparisonStats.js
+++ src/container/ComparisonStats.js
@@ -9,26 +9,39 @@ class ComparisonStats extends Component {
         let profileStats = this.context.globalStats
         let friendStats = this.props.friend_stats
         let friendName = friendStats[0].username
-
+        console.log('comparison stats,friends',friendStats)
         let profileTotal = (
-            profileStats.beer +
+            (profileStats.beer +
             profileStats.seltzer +
             profileStats.craft_beer +
             profileStats.wine +
             profileStats.shots +
-            profileStats.cocktail
+            profileStats.cocktail) +
+            (friendStats[1].duce +
+                friendStats[1].eight_n_up +
+                friendStats[1].beer_bong +
+                friendStats[1].shotgun +
+                friendStats[1].wine_flight +
+                friendStats[1].beer_flight)
         )
 
         let friendTotal = (
-            friendStats[0].beer +
+            (friendStats[0].beer +
             friendStats[0].seltzer +
             friendStats[0].craft_beer +
             friendStats[0].wine +
             friendStats[0].shots +
-            friendStats[0].cocktail
+            friendStats[0].cocktail) +
+            (friendStats[1].duce +
+                friendStats[1].eight_n_up +
+                friendStats[1].beer_bong +
+                friendStats[1].shotgun +
+                friendStats[1].wine_flight +
+                friendStats[1].beer_flight)
         )
 
        let compare
+       console.log('compare',profileTotal,friendTotal)
        if(profileTotal >= friendTotal){
            compare = profileTotal - friendTotal
        } else if(friendTotal >= profileTotal){
