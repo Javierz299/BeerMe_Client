19c8291a4fa8825e17adb7377941bbcf085cd571
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-10-22 12:46:49 -0400

    conditional for those with who dont have an intial submit for second drink form

--- src/container/DetailedStats.js
+++ src/container/DetailedStats.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import * as ACTIONS from '../store/actions/actions'
 
@@ -24,6 +24,7 @@ class DetailedStats extends Component {
     }
 
     render() {
+        console.log('friendstats',this.props.friend_stats)
         return (
                  <div id="detailed-stats" onClick={() => this.hide_friend_stats()}>
                     <h3 id="drink-stats">{this.props.friend_stats[0].username}</h3>
@@ -36,12 +37,17 @@ class DetailedStats extends Component {
                         <div><h4 className="friend-stat"><img src={shotPic} alt="shot pic" /> {this.props.friend_stats[0].shots}</h4></div>
                         <div><h4 className="friend-stat"><img src={mixedPic} alt="mixed pic" /> {this.props.friend_stats[0].cocktail}</h4></div>
 
+                    {!this.props.friend_stats[1] ? 
+                    null :
+                        <Fragment>
                         <div><h4 className="friend-stat"><img src={Duce} alt="duce" /> {this.props.friend_stats[1].duce}</h4></div>
                         <div><h4 className="friend-stat"><img src={Eight} alt="eight perc" /> {this.props.friend_stats[1].eight_n_up}</h4></div>
                         <div><h4 className="friend-stat"><img src={BeerBong} alt="beer bong" /> {this.props.friend_stats[1].beer_bong}</h4></div>
                         <div><h4 className="friend-stat"><img src={Shotgun} alt="shotgun" /> {this.props.friend_stats[1].shotgun}</h4></div>
                         <div><h4 className="friend-stat"><img src={WineFlight} alt="wine flight" /> {this.props.friend_stats[1].wine_flight}</h4></div>
                         <div><h4 className="friend-stat"><img src={BeerFlight} alt="beer flight" /> {this.props.friend_stats[1].beer_flight}</h4></div>
+                        </Fragment>
+                    }
                     </div>
                  </div>   
         )
